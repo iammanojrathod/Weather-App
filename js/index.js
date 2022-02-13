6465166502bb3a026d6b4b40cb8ec56c281bffde
Manoj Rathod <rathodmanoj247@gmail.com>
2022-02-13 23:08:44 +0530

    initial commit

---
+++ js/index.js
@@ -0,0 +1,51 @@
+const input = document.querySelector(".search");
+const button = document.querySelector(".search_btn");
+
+const weatherApi = {
+    key: "f24f9b1151ddc0a3d2d32aa40009e5cd",
+    baseUrl: "https://api.openweathermap.org/data/2.5/weather", 
+}
+
+// Event Listener Function on keypress
+input.addEventListener('keypress', (event) => {
+    if(event.keyCode === 13) {
+        getWeatherReport(input.value);
+    } 
+});
+
+button.addEventListener('click', () => {
+    getWeatherReport(input.value);
+})
+
+// Get Weather Report
+function getWeatherReport(city) {
+    document.body.style.backgroundImage = `url(https://source.unsplash.com/1600x900/?${city})`;
+
+    fetch(`${weatherApi.baseUrl}?q=${city}&appid=${weatherApi.key}&units=metric`)
+    .then(weather => {
+        return weather.json();
+    }).then(showWeatherReport);
+}
+
+// Show Weather Report
+function showWeatherReport(weather){
+
+    const city = document.getElementById('city_name');
+    city.innerText = `Weather in ${weather.name}, ${weather.sys.country}`;
+
+    const temperature = document.getElementById('temp');
+    temperature.innerHTML = `${Math.round(weather.main.temp)}&deg;C`;
+
+    const weatherType = document.getElementById('desc');
+    weatherType.innerText = `${weather.weather[0].main}`;
+
+    const humidity = document.querySelector("#humid");
+    humidity.innerHTML = `Humidity: ${weather.main.humidity}%`
+
+    const wind = document.querySelector("#wind");
+    wind.innerHTML = `Wind Speed: ${Math.round(weather.wind.speed)} km/ph`
+    
+    const icon = document.querySelector(".icon");
+    icon.src = `https://openweathermap.org/img/wn/${weather.weather[0].icon}.png`;
+
+}
